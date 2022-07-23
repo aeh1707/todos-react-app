1c18a3bc5f041d390d2bb86ff249e335f9f4ac30
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-23 10:48:27 +0100

    Add components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const element = <h1>Hello from Create React App</h1>;
-
-ReactDOM.render(element, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
